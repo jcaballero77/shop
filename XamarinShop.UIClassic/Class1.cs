e5e638e2186e7f425b70704a8979ebc00ed56068
jcaballero77 <jcaballero77@hotmail.com>
2021-08-09 20:39:41 -0600

    Cambios al 9 agosto Controles

---
+++ XamarinShop.UIClassic/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace XamarinShop.UIClassic
+{
+    public class Class1
+    {
+    }
+}
---
+++ XamarinShop.Web/Data/DataContext.cs
@@ -0,0 +1,18 @@
+﻿namespace XamarinShop.Web.Data
+{
+    using Microsoft.EntityFrameworkCore;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using XamarinShop.Web.Data.Entities;
+
+    public class DataContext:DbContext 
+    {
+        public DbSet<Product>  Products{ get; set; }
+        public DataContext(DbContextOptions<DataContext> options) : base (options)
+        { 
+                    
+        }
+    }
+}
