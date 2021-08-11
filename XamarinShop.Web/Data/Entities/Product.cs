6137d95f6ff2c1d10f414981ad82f6636e8c3eb6
jcaballero77 <jcaballero77@hotmail.com>
2021-08-11 17:52:01 -0600

    Implementar repository Pattern

--- XamarinShop.Web/Data/Entities/Product.cs
+++ XamarinShop.Web/Data/Entities/Product.cs
@@ -3,11 +3,12 @@
 	using System;
 	using System.ComponentModel.DataAnnotations;
 	public class Product
-    {
-		
+	{
+
 
 		public int Id { get; set; }
-
+		[MaxLength(50,ErrorMessage ="El campo {0} solo puede contener {1} caracteres de largo")]
+		[Required]
 		public string Name { get; set; }
 
 		[DisplayFormat(DataFormatString = "{0:C2}", ApplyFormatInEditMode = false)]
@@ -17,10 +18,10 @@
 		public string ImageUrl { get; set; }
 
 		[Display(Name = "Last Purchase")]
-		public DateTime LastPurchase { get; set; }
+		public DateTime? LastPurchase { get; set; }
 
 		[Display(Name = "Last Sale")]
-		public DateTime LastSale { get; set; }
+		public DateTime? LastSale { get; set; }
 
 		[Display(Name = "Is Availabe?")]
 		public bool IsAvailabe { get; set; }
